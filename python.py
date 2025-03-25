32c972aa2cbbf9d91d8fd655f8c8cb2a2677af02
shriyash2904 <shriyashj20@gmail.com>
2025-03-25 16:41:45 +0530

    Update python.py

--- python.py
+++ python.py
@@ -5,17 +5,3 @@ highest_sales = max(sales)
 lowest_sales = min(sales)
 print("Highest Sales:", highest_sales)
 print("Lowest Sales:", lowest_sales)
-
-
-
-import numpy as np
-sales_data = np.array([1000, 1200, 1500, 1800, 2000, 2100, 2500, 2700])
-sales_data_2d = sales_data.reshape(4, 2)
-print(sales_data_2d)
-
-
-
-
-
-
-
