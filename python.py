25c1148650a47d6c046568a3f780df1240e74330
shriyash2904 <shriyashj20@gmail.com>
2025-03-25 16:40:22 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+sales = [5000, 7000, 8000, 6500, 7200, 9000, 8500]
+highest_sales = max(sales)
+lowest_sales = min(sales)
+print("Highest Sales:", highest_sales)
+print("Lowest Sales:", lowest_sales)
+
+
+# In[2]:
+
+
+import numpy as np
+sales_data = np.array([1000, 1200, 1500, 1800, 2000, 2100, 2500, 2700])
+sales_data_2d = sales_data.reshape(4, 2)
+print(sales_data_2d)
+
+
+# In[ ]:
+
+
+
+
